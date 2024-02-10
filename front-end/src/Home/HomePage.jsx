d215eb54ae2ca4f7a6328ccbd488135810995cbf
shahmir-m <shahmirnation@gmail.com>
2024-02-09 20:59:16 -0600

    init commit - contains image for home page

--- front-end/src/Home/HomePage.jsx
+++ front-end/src/Home/HomePage.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
-
+import mainImage from"../assets/homemainimage.jpg";
 const HomePage = () => {
+  const containerStyle = {
+    display: 'flex',
+    alignItems: 'center', 
+    justifyContent: 'space-between', 
+    padding: '20px', 
+  };
+
+  const textStyle = {
+    flex: 1,
+    marginRight: '20px', 
+  };
+
+  const imageStyle = {
+    width: '50%', // Sets the image width to 50% of the container
+    height: 'auto', 
+  };
+
   return (
-    <div>
-      <h2>Home</h2>
-      <p>This is the home page.</p>
+    <div style={containerStyle}>
+      <div style={textStyle}>
+        <h2>Texas Homes Project</h2>
+        <p>Test.</p>
+      </div>
+      <img src={mainImage} alt="test" style={imageStyle} />
     </div>
   );
 }
