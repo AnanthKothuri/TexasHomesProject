68553016d8c5ed5da7f40232baf4e559dd22a21b
shahmir-m <shahmirnation@gmail.com>
2024-02-09 22:28:17 -0600

    updated homepage with list of models, description, and place holder image

--- front-end/src/Home/HomePage.jsx
+++ front-end/src/Home/HomePage.jsx
@@ -1,30 +1,88 @@
 import React from 'react';
-import mainImage from"../assets/homemainimage.jpg";
-const HomePage = () => {
-  const containerStyle = {
+import mainImage from "../assets/homemainimage.jpg";
+import { MdNightShelter } from "react-icons/md";
+import { FaHandsHelping } from "react-icons/fa";
+import { FaLandmarkFlag } from "react-icons/fa6";
+
+const Feature = ({ smalltext, text, icon, iconBg }) => {
+  const stackStyle = {
     display: 'flex',
-    alignItems: 'center', 
-    justifyContent: 'space-between', 
-    padding: '20px', 
+    alignItems: 'center',
+    gap: '8px',
+  };
+
+  const flexStyle = {
+    width: '32px',
+    height: '32px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: '50%',
+    backgroundColor: iconBg,
   };
 
   const textStyle = {
-    flex: 1,
-    marginRight: '20px', 
+    fontWeight: 600,
+  };
+
+  const smallTextStyle = {
+    fontWeight: 200,
+  };
+
+  return (
+    <div style={stackStyle}>
+      <div style={flexStyle}>{icon}</div>
+      <span style={textStyle}>{text}</span>
+      <span style={smallTextStyle}>{smalltext}</span>
+    </div>
+  );
+};
+
+const HomePage = () => {
+  const containerStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: '45px 150px 100px 150px',
+    boxSizing: 'border-box',
+    height: '100vh',
+  };
+
+  const textAndFeatureContainerStyle = {
+    maxWidth: '600px', // Adjust based on your content's needs
+    textAlign: 'left',
   };
 
   const imageStyle = {
-    width: '50%', // Sets the image width to 50% of the container
-    height: 'auto', 
+    marginLeft: '20px',
+    width: '50%',
+    height: 'auto',
   };
 
   return (
     <div style={containerStyle}>
-      <div style={textStyle}>
-        <h2>Texas Homes Project</h2>
-        <p>Test.</p>
+      <div style={textAndFeatureContainerStyle}>
+        <h2 style={{ marginBottom: '20px' }}>Texas Homes Project</h2>
+        <p>Texas Homes Project hopes to assist individuals experiencing homelessness in Texas. We hope to provide an internet platform that can connect people in need to homeless shelters, raise awareness for organizations working to assist homeless populations, and highlight local volunteer opportunities to aid homeless individuals.</p>
+        <Feature
+          icon={<MdNightShelter />}
+          iconBg="#FFFFFF"
+          text="Shelters"
+          smalltext="There are X homeless shelters in the state of Texas."
+        />
+        <Feature
+          icon={<FaLandmarkFlag />}
+          iconBg="#FFFFFF"
+          text="Counties"
+          smalltext="There are X counties in the state of Texas."
+        />
+        <Feature
+          icon={<FaHandsHelping/>}
+          iconBg="#FFFFFF"
+          text="Events"
+          smalltext="There are X upcoming volunteer events."
+        />
       </div>
-      <img src={mainImage} alt="test" style={imageStyle} />
+      <img src={mainImage} alt="Descriptive Alt Text" style={imageStyle} />
     </div>
   );
 }
