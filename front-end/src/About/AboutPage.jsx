3287bf48d3d91496bac093751b5219ad08f33d21
gabe-casanova <gabecasanova7@gmail.com>
2024-02-11 20:04:38 -0600

    testing process.env.secrets

--- front-end/src/About/AboutPage.jsx
+++ front-end/src/About/AboutPage.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 
 const AboutPage = () => {
+
+  // Parse the secrets JSON string
+  let secrets = process.env.secrets;
+  if (secrets) {
+    try {
+      secrets = JSON.parse(secrets);
+      console.log(secrets);
+    } catch (error) {
+      console.error('Error parsing secrets JSON:', error);
+    }
+  }
+
   return (
     <div>
       <h2>About</h2>
