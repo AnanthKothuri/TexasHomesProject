fd549f2fa765fee5a2ef1b2e82f09825227dae5a
gabe-casanova <gabecasanova7@gmail.com>
2024-02-11 20:18:56 -0600

    add console.log to test

--- front-end/src/About/AboutPage.jsx
+++ front-end/src/About/AboutPage.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 const AboutPage = () => {
 
+  console.log('hi');
+
   // Parse the secrets JSON string
+  let env = process.env;
+  console.log(env);
   let secrets = process.env.secrets;
   if (secrets) {
     try {
       secrets = JSON.parse(secrets);
       console.log(secrets);
+      console.log('made it here');
     } catch (error) {
       console.error('Error parsing secrets JSON:', error);
     }
