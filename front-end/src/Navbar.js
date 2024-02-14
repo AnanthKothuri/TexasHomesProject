29e32bc160e48ca14dc71ffbf248168f9b8334b9
gabe-casanova <gabecasanova7@gmail.com>
2024-02-14 14:50:46 -0600

    added logo to navbar

--- front-end/src/Navbar.js
+++ front-end/src/Navbar.js
@@ -1,27 +1,33 @@
-import './App.css'
-import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import "./App.css";
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { logo_info } from "./data/logo";
 
 function NavBar() {
-    return (
-
-      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="/">Texas Homes Project</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/about">About</Nav.Link>
-              <Nav.Link href="/shelters">Shelters</Nav.Link>
-              <Nav.Link href="/counties">Counties</Nav.Link>
-              <Nav.Link href="/events">Events</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  }
-  
+  return (
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <img
+          src={logo_info.src}
+          alt={logo_info.alt_text}
+          height="30"
+          className="d-inline-block align-top"
+          style={{ marginRight: 3 }}
+        />
+        <Navbar.Brand href="/">Texas Homes Project</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/shelters">Shelters</Nav.Link>
+            <Nav.Link href="/counties">Counties</Nav.Link>
+            <Nav.Link href="/events">Events</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
 
 export default NavBar;
---
+++ front-end/src/data/logo.js
@@ -0,0 +1,6 @@
+import logo_img from "../assets/transparent-tx-house-logo.png";
+
+export const logo_info = {
+    src: logo_img,
+    alt_text: "Texas Homes Project Logo"
+}
