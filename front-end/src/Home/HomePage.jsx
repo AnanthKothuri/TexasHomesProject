df0ccb833438a3ab00586a66edf0c7ce94a64f5e
gabe-casanova <gabecasanova7@gmail.com>
2024-02-14 15:32:09 -0600

    added color and restylized the 'At a glance' section of homepage

--- front-end/src/Home/HomePage.jsx
+++ front-end/src/Home/HomePage.jsx
@@ -26,30 +26,39 @@ const HomePage = () => {
 
       <Row md='auto' style={{margin: 80, justifyContent: 'center'}}>
         <Col>
-          <p style={{width: 550}}>As of 2023, nearly 67,000 people in the state of Texas alone fell into homelessness. Of those that are homeless, around 20% are unsheltered, meaning they have no place to go our
-          sleep through the night. These are enormous numbers and are things we sometimes overlook as we go about 
-          our daily lives.</p>
-          <p style={{width: 550}}>Texas Homes Project hopes to assist individuals experiencing homelessness in Texas. 
-              We hope to provide an internet platform that can connect people in need to homeless shelters, raise awareness for organizations working to assist homeless populations, 
-              and highlight local volunteer opportunities to aid homeless individuals.</p>
+          <p style={{width: 550}}>
+            As of 2023, nearly 67,000 people in the state of Texas alone fell into homelessness. 
+            Of those that are homeless, around 20% are unsheltered, meaning they have no place to
+            go our sleep through the night. These are enormous numbers and are things we sometimes
+            overlook as we go about our daily lives.
+          </p>
+          <p style={{width: 550}}>
+            Texas Homes Project hopes to assist individuals experiencing homelessness in Texas. 
+            We hope to provide an internet platform that can connect people in need to homeless
+            shelters, raise awareness for organizations working to assist homeless populations, 
+            and highlight local volunteer opportunities to aid homeless individuals.
+          </p>
         </Col>
 
-        <Col>
+        <Col style={{ fontSize: '1.2em' }}>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <p style={{ marginBottom: 5, fontSize: 25 }}>
+              At a glance,
+            </p>
+          </div>
+          {/* Display info for "Shelters", "Counties", "Events" with icons */}
           <Feature
-              icon={<MdNightShelter />}
-              iconBg="#FFFFFF"
+              icon={<MdNightShelter style={{ color: "#0b2b68", fontSize: '1.5em' }}/>}
               text="Shelters"
-              smalltext="There are X homeless shelters in the state of Texas."
+              smalltext="There are X homeless shelters in Texas."
             />
             <Feature
-              icon={<FaLandmarkFlag />}
-              iconBg="#FFFFFF"
+              icon={<FaLandmarkFlag style={{ color: "#c91e40", fontSize: '1.2em' }}/>}
               text="Counties"
-              smalltext="There are X counties in the state of Texas."
+              smalltext="There are X counties in Texas."
             />
             <Feature
-              icon={<FaHandsHelping />}
-              iconBg="#FFFFFF"
+              icon={<FaHandsHelping style={{ color: "#1d66b5", fontSize: '1.3em' }}/>}
               text="Events"
               smalltext="There are X upcoming volunteer events."
             />
