120ae4174de10df35721ee0a42b3ee9ab718844a
gabe-casanova <gabecasanova7@gmail.com>
2024-02-22 21:52:46 -0600

    refactored AboutPage and PageLayout

--- front-end/src/About/AboutPage.jsx
+++ front-end/src/About/AboutPage.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { contributors } from "../data/about";
-import PageLayout from '../ModelTemplates/PageLayout';
+import { Card } from 'react-bootstrap'
+import { contributors, tools_used } from "../data/about";
+import InstanceCard from '../ModelTemplates/InstanceCard.jsx'
 
 const AboutPage = () => {
 
@@ -39,6 +40,9 @@ const AboutPage = () => {
       });
   }, [PROJECT_ID, GITLAB_TOKEN]);
 
+  let totalCommits = 0;
+  let totalIssues = 0;
+
   const data = {
     aboutPage: contributors.map((contributor) => {
       // teamMemberData only includes data for 1 team member (based on email)
@@ -48,11 +52,13 @@ const AboutPage = () => {
       
       // numCommits
       const num_commits = teamMemberData ? teamMemberData.commits : 0;
+      totalCommits += num_commits;
 
       // numIssues
       const num_issues = issueResults.filter(
         (issue) => issue.author.username === contributor.gitlab_username
       ).length;
+      totalIssues += num_issues;
 
       // append new entry for each team member
       return {
@@ -62,13 +68,38 @@ const AboutPage = () => {
       };
     })
   };
+  
+  // useful for rending totalCommits + totalIssues
+  const renderGitlabStat = (label, value) => (
+    <>
+      <b>{label}: </b>
+      <span style={{ color: "#2e5e9e" }}>{value}</span>
+    </>
+  );
+
+  const renderToolsUsed = (tools_used) => {
+    return (
+      <div className="row row-cols-auto" style={{ display: 'flex', justifyContent: 'center' }}>
+        {tools_used.map((tool, index) => (
+          <Card key={index} className='card-content mb-4 shadow-sm' style={{ width: 200, padding: 7.5, paddingTop: 20, marginRight: 23 }}>
+            <Card.Img variant="top" src={tool.src} style={{ height: 100, objectFit: 'contain', width: '100%', padding: 5 }} />
+            <Card.Body>
+              <Card.Title style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: '1.4em' }}>
+                <b style={{ paddingTop: 7 }}>{tool.name}</b>
+              </Card.Title>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+    );
+  }
 
   return (
     <>
       {/* About Us Description */}
       <div className="container text-center">
         <header className="page-header" style={{ fontSize: 30, padding: 30, paddingBottom: 15 }}>
-            About Us
+          About Us
         </header>
         <p style={{ fontSize: '1.5em', paddingLeft: 120, paddingRight: 120 }}>
           We're on a mission to make it easier for people experiencing homelessness to receive the
@@ -80,7 +111,44 @@ const AboutPage = () => {
           resources when presented with an opportunity of helping someone suffering from homeslessness.
         </p>
       </div>
-      <PageLayout data={data.aboutPage} pageTitle={"Meet the Team"}/>
+
+      {/* Team Member Cards */}
+      <header className="container text-center page-header" style={{fontSize: 30, padding: 30}}>
+        Meet the Team
+      </header>
+      <div className="row row-cols-auto" style={{justifyContent: 'center'}}>
+        {data.aboutPage.map((item) => (
+            <div className="col" key={item.name}>
+              <InstanceCard item={item} type={"Meet the Team"} />
+            </div>
+          ))
+        }
+      </div>
+
+      {/* Total Stats */}
+      <div className="container text-center" style={{display: 'block'}}>
+        <header className="page-header" style={{ fontSize: 30, padding: 30, paddingBottom: 15 }}>
+          Total Stats
+        </header>
+        <p style={{ fontSize: '1.2em', paddingLeft: 120, paddingRight: 120, fontFamily: 'monospace' }}>
+          {renderGitlabStat("Total commits", totalCommits)}
+          <br/>
+          {renderGitlabStat("Total issues", totalIssues)}
+        </p>
+      </div>
+
+      {/* Tools Used */}
+      <div className="container text-center" style={{display: 'block'}}>
+        <header className="page-header" style={{ fontSize: 30, padding: 30, paddingBottom: 15 }}>
+          Tools Used
+        </header>
+        {renderToolsUsed(tools_used)}
+      </div>
+
+      {/* Footer */}
+      <div className="container text-center" style={{padding: 30, paddingBottom: 50}}>
+        {data.aboutPage.length} out of {data.aboutPage.length} • {"About Us"}
+      </div>
     </>
   );
 }
