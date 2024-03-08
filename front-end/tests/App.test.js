487cf390d095523532115a9dff2c8d8794933aa3
shahmir-m <shahmirnation@gmail.com>
2024-03-07 18:12:49 -0600

    retrying pipeline 2

--- front-end/tests/App.test.js
+++ front-end/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react';
-import App from '../../src/App.js';
+import App from '../src/App.js';
 
 //frontend test suite
     describe('Frontend Tests', () => {
