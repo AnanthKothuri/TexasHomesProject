8cf02f66fe51b0588751cc5fd8053a498957ffa6
Ananth Kothuri <ananth.s.kothuri@gmail.com>
2024-04-03 14:17:03 -0500

    made small user story changes

--- front-end/src/assets/Colors.js
+++ front-end/src/assets/Colors.js
@@ -9,7 +9,10 @@ const Colors = {
     darkGray: "#5F5F5F",
     gray: "#838383",
     white: "#FFFFFF",
-    black: "#000000"
+    black: "#000000",
+    blue: "#3380FF",
+    yellow: "#FADA35",
+    red: "#E25C5C",
 };
 
 export default Colors;
