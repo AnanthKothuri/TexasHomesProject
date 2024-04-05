4689053b44f85cd39e5b900831ccfda63f8918e6
gabe-casanova <gabecasanova7@gmail.com>
2024-04-05 01:19:39 -0500

    import React into NavBar.jsx

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { Navbar, Nav, Container, Button } from "react-bootstrap";
 import { FaSearch } from "react-icons/fa";
