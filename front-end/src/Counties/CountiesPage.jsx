661b2953ef2c0b7cb0d7641d33c00fc3d42dbc98
jerodvo <jerodvo@utexas.edu>
2024-04-04 22:34:03 -0500

    Added initial sorting functionality

--- front-end/src/Counties/CountiesPage.jsx
+++ front-end/src/Counties/CountiesPage.jsx
@@ -1,17 +1,11 @@
 import PageLayout from '../ModelTemplates/PageLayout';
-//import data from '../data/countyData.json';
-import useFetchAll from '../hooks/usefetchAll';
-import LoadingPage from '../components/LoadingPage';
 
 
 
 const CountiesPage = () => {
-  const { data: counties, loading, error } = useFetchAll('https://api.texashomesproject.me/counties/');
+  //const { data: counties, loading, error } = useFetchAll('https://api.texashomesproject.me/counties/', { sort_by: '' });
 
-  if (loading) return <LoadingPage/>;
-  if (error) return <div>Error: {error}</div>;
-
-  return <PageLayout data={counties} pageTitle={"Counties"} />;
+  return <PageLayout pageTitle={"Counties"} />;
 }
 
 export default CountiesPage;
